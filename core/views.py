95202e2793d88344c1a83243b9ff8a8f812ee4ca
mahrezbh <mahrez.benhamad@gmail.com>
2021-02-18 22:39:28 +0100

    first commit

--- core/views.py
+++ core/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-# @login_required
 def home(request):
     return render(request, 'home.html')
 
-# @login_required
 def setup(request):
     return render(request, 'setup.html')
 
-# @login_required
 def supported(request):
     return render(request, 'supported.html')
